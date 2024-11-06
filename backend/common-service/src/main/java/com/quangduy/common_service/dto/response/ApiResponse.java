3467d378eadd1a68293fa9d4bdf1e458b3c74a6e
QuangDuy212 <duy2k4ml1234@gmail.com>
2024-11-06 07:49:10 +0700

    init project

---
+++ backend/common-service/src/main/java/com/quangduy/common_service/dto/response/ApiResponse.java
@@ -0,0 +1,22 @@
+package com.quangduy.common_service.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse {
+    int statusCode;
+    String message;
+    Object data;
+}
